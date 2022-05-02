81cdb4441a548b91c23106ff0b70640a7244337e
Denis Crasnean <deniscrasnean@gmail.com>
2022-05-02 13:14:15 +0300

    Added the base domain models with basic configuration

---
+++ src/Entity/Meta.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MetaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MetaRepository::class)
+ */
+class Meta
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private string $type;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private string $key;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private string $value;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function setKey(string $key): self
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
